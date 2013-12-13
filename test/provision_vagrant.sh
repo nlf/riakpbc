56431213f1038a38499a31b5b44ec76774f9df3a
Noah Isaacson <noah@nisaacson.com>
2013-12-13 11:10:51 -0700

    Change: use docker to provision riak in vagrant.
    Add: support for vmware_fusion provider as well as virtualbox in Vagrantfile.
    Rebuild binary modules in vagrant vm during provisioning.

--- test/provision_vagrant.sh
+++ test/provision_vagrant.sh
@@ -41,43 +41,9 @@ function install_node {
   exit 1
 }
 
-function install_riak {
-  echo "-----> Install riak if needed"
-  command -v riak > /dev/null
-  if [[ $? -eq 0 ]]; then
-    echo "       riak already installed"
-    return
-  fi
-  echo "       riak not installed, begin installation from ppa now"
-  curl http://apt.basho.com/gpg/basho.apt.key | sudo apt-key add -
-  sudo bash -c "echo deb http://apt.basho.com $(lsb_release -sc) main > /etc/apt/sources.list.d/basho.list"
-  sudo apt-get update
-  sudo apt-get install riak
-  command -v riak > /dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "       riak failed to install correctly"
-    exit 1
-  fi
-
-  # switch to leveldb as the riak backend
-  echo "change riak backend to leveldb to support secondary indices"
-  cd /etc/riak &&  sudo sed -i.bak -e s/riak_kv_bitcask_backend/riak_kv_eleveldb_backend/g app.config
-  cd /etc/riak &&  sudo sed -i.bak -e 0,/"enabled, false"/{s/"enabled, false"/"enabled, true"/} app.config
-  sudo riak start
-  if [[ $? -ne 0 ]]; then
-    echo "      riak failed to start correctly"
-    exit 1
-  fi
-  echo "      riak start completed"
-  sleep "5s"
-  riak ping
-  if [[ $? -ne 0 ]]; then
-    echo "       riak failed to install correctly"
-    exit 1
-  fi
-   echo "riak install correctly"
+function npm_rebuild {
+  su -c "cd /vagrant && npm rebuild" -s /bin/sh "vagrant"
 }
-
 install_from_apt "curl"
 install_node
-install_riak
+npm_rebuild
