1e0ff7dbea12a11f02b774859c5bbcde6cdc2be7
Noah Isaacson <noah@nisaacson.com>
2013-12-13 23:04:26 -0700

    Bugfix: emit errors correctly for streaming apis.

---
+++ test/streaming-error-test.js
@@ -0,0 +1,36 @@
+var chai = require('chai');
+chai.Assertion.includeStack = true; // defaults to false
+var expect = chai.expect;
+
+var riakpbc = require('../index');
+
+describe('Streaming error test', function () {
+    it('should output error events when using a streaming api', function (done) {
+        var invalidPort = 1111;
+        var clientOpts = {
+            host: 'localhost',
+            port: invalidPort,
+            timeout: 20 // 20 milliseconds to make test run faster
+        };
+        var client = riakpbc.createClient(clientOpts);
+        var queryOpts = {
+            queryType: 1, // range query type
+            range_min: 0,
+            range_max: 999999,
+            index: 'name_bin',
+            bucket: 'test'
+        };
+        var streaming = true;
+        var readStream = client.getIndex(queryOpts, streaming);
+        readStream.on('error', errorHandler);
+
+        function errorHandler(err) {
+            expect(err).to.exist;
+            expect(err.message).to.equal('Connection timeout');
+            done();
+        }
+
+
+    });
+});
+
