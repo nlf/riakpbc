414ccb2c9a04fa135058ef0aa58f9c517113364f
Noah Isaacson <noah@nisaacson.com>
2014-01-02 13:12:35 -0700

    Add: test for search behaviour.
    The current implementation does not return the num_found value from
    riak. This commit adds an explict test for the search api output from the client.
    
    Also cleanup Vagrantfile and provisioning to support search testing.
    (search needs to enabled on the search_test bucket).

--- test/provision_vagrant.sh
+++ test/provision_vagrant.sh
@@ -9,14 +9,7 @@ function install_from_apt {
     return
   fi
   echo "       $TOOL not installed, begin installation from apt"
-  sudo apt-get install -qqy curl
-  command -v $TOOL > /dev/null
-  if [[ $? -eq 0 ]]; then
-    echo "       $TOOL installed correctly"
-    return
-  fi
-  echo "$TOOL failed to install correctly"
-  exit 1
+  sudo apt-get install -qqy $TOOL
 }
 
 function install_node {
@@ -28,7 +21,7 @@ function install_node {
   fi
   echo "       nodejs not installed, begin installation from ppa now"
   sudo apt-get update
-  sudo apt-get install -y -qq python-software-properties python g++ make
+  sudo apt-get install -y -qq python-software-properties
   sudo add-apt-repository -y ppa:chris-lea/node.js
   sudo apt-get update -y
   sudo apt-get install -qqy nodejs
@@ -41,5 +34,32 @@ function install_node {
   exit 1
 }
 
+function install_httpie {
+  echo "-----> Install python"
+  command -v http > /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "       httpie already installed"
+    return
+  fi
+  echo "-----> Install pip"
+  sudo apt-get install -qqy python-pip python-dev build-essential
+  sudo pip install --upgrade pip
+  echo "-----> Install httpie"
+  sudo pip install --upgrade httpie
+}
+
+function enable_indexing_on_riak_buckets {
+  echo "-----> Enable search on riak buckets"
+  echo "       sleep for 3 seconds to give riak time to start"
+  sleep "3s"
+  BUCKET="search_test"
+  URL="http://localhost:8098/riak/$BUCKET"
+  DATA='{"props":{"precommit":[{"mod":"riak_search_kv_hook","fun":"precommit"}]}}'
+  curl --silent -X PUT -H 'Content-Type: application/json' -d "$DATA" $URL
+  echo "      enabled search on bucket: $BUCKET"
+}
+
 install_from_apt "curl"
 install_node
+enable_indexing_on_riak_buckets
+
---
+++ test/search-test.js
@@ -0,0 +1,78 @@
+
+var _ = require('lodash-node');
+var chai = require('chai');
+chai.Assertion.includeStack = true; // defaults to false
+
+var expect = chai.expect;
+var inspect = require('eyespect').inspector();
+var q = require('q');
+var sinon = require('sinon');
+
+var riakpbc = require('../index');
+var client = riakpbc.createClient({ host: 'localhost', port: 8087 });
+
+var numRows = 10;
+var bucket = 'search_test';
+describe('search', function searchSuite() {
+    this.slow('1s');
+    before(function beforeBlock(done) {
+        var promise = connectClient();
+        promise.then(setupFixtures).nodeify(done);
+    });
+
+    after(function afterBlock(done) {
+        this.timeout('10s');
+        return done();
+    });
+
+    it('should return search results', function (done) {
+        var opts = {
+            q: 'bar:value_*',
+            index: bucket,
+            rows: 1,
+            start: 0
+        };
+        client.search(opts, cb);
+
+        function cb(err, reply) {
+            expect(err).to.not.exist;
+            expect(reply, 'should get search reply').to.exist;
+            expect(reply).to.have.ownProperty('docs');
+            expect(reply).to.have.ownProperty('max_score');
+            expect(reply.docs).to.be.an('array');
+            expect(reply.docs.length).to.equal(opts.rows);
+            expect(reply, 'num_found field missing in reply').to.have.ownProperty('num_found');
+            expect(reply.num_found).to.be.above(opts.rows);
+            done();
+        }
+    });
+});
+
+function connectClient() {
+    var promise = q.ninvoke(client, 'connect');
+    return promise;
+}
+
+function setupFixtures() {
+    var promises = _.range(0, numRows).map(createRow);
+    return q.all(promises);
+}
+
+function createRow(id) {
+    var key = [id, 'key'].join('_');
+    var value = {
+        bar: 'value_' + id
+    };
+    var content = {
+        value: JSON.stringify(value),
+        content_type: 'application/json'
+    };
+    var saveOpts = {
+        bucket: bucket,
+        content: content,
+        key: key
+    };
+
+    var promise = q.ninvoke(client, 'put', saveOpts);
+    return promise;
+}
