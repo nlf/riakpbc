786b120113cdd1f02ba6b69a8459c5f61d4d8a54
Nathan LaFreniere <quitlahok@gmail.com>
2014-09-15 15:57:11 -0700

    write lots of map tests

--- test/maps.js
+++ test/maps.js
@@ -9,12 +9,541 @@ var expect = Lab.expect;
 var it = lab.test;
 
 var client = RiakPBC.createClient();
+var context;
 
 describe('Maps', function () {
 
     describe('(callbacks)', function () {
+
+        it('can create a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        adds: [{
+                            name: '_counter',
+                            type: 1
+                        }, {
+                            name: '_set',
+                            type: 2
+                        }, {
+                            name: '_register',
+                            type: 3
+                        }, {
+                            name: '_flag',
+                            type: 4
+                        }, {
+                            name: '_map',
+                            type: 5
+                        }]
+                    }
+                }
+            }, function (err) {
+
+                expect(err).to.not.exist;
+                done();
+            });
+        });
+
+        it('can get a map', function (done) {
+
+            client.getCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map'
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                expect(reply).to.have.property('type', 3);
+                expect(reply).to.have.property('value').that.is.an('object');
+                done();
+            });
+        });
+
+        it('can update a counter within a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_counter',
+                                type: 1
+                            },
+                            counter_op: {
+                                increment: 5
+                            }
+                        }]
+                    }
+                },
+                return_body: true
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(1);
+                expect(reply.map_value[0]).to.have.deep.property('field.name', '_counter');
+                expect(reply.map_value[0]).to.have.deep.property('field.type', 1);
+                expect(reply.map_value[0]).to.have.property('counter_value');
+                expect(reply.map_value[0].counter_value.toNumber()).to.equal(5);
+                done();
+            });
+        });
+
+        it('can update a set within a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_set',
+                                type: 2
+                            },
+                            set_op: {
+                                adds: ['one', 'two']
+                            }
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(2);
+                expect(reply.map_value).to.include({ field: { name: '_set', type: 2 }, set_value: ['one', 'two'] });
+                done();
+            });
+        });
+
+        it('can update a register within a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_register',
+                                type: 3
+                            },
+                            register_op: 'testing'
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(3);
+                expect(reply.map_value).to.include({ field: { name: '_register', type: 3 }, register_value: 'testing' });
+                done();
+            });
+        });
+
+        it('can update a flag within a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_flag',
+                                type: 4
+                            },
+                            flag_op: 1
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(4);
+                expect(reply.map_value).to.include({ field: { name: '_flag', type: 4 }, flag_value: true });
+                done();
+            });
+        });
+
+        it('can update a map within a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_map',
+                                type: 5
+                            },
+                            map_op: {
+                                updates: [{
+                                    field: {
+                                        name: '_test',
+                                        type: 4
+                                    },
+                                    flag_op: 2
+                                }]
+                            }
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            }, function (err, reply) {
+            
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(5);
+                expect(reply.map_value).to.include({ field: { name: '_map', type: 5 }, map_value: [{ field: { name: '_test', type: 4 }, flag_value: false }] });
+                done();
+            });
+        });
+
+        it('can remove a field from a map', function (done) {
+
+            client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        removes: [{
+                            name: '_map',
+                            type: 5
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            }, function (err, reply) {
+
+                expect(err).to.not.exist;
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(4);
+                expect(reply.map_value).to.not.include({ field: { name: '_map', type: 5 }, map_value: [{ field: { name: '_test', type: 4 }, flag_value: false }] });
+                done();
+            });
+        });
+
+        after(function (done) {
+
+            client.del({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map'
+            }, function (err) {
+
+                expect(err).to.not.exist;
+                done();
+            });
+        });
     });
 
     describe('(streams)', function () {
+
+        it('can create a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        adds: [{
+                            name: '_counter',
+                            type: 1
+                        }, {
+                            name: '_set',
+                            type: 2
+                        }, {
+                            name: '_register',
+                            type: 3
+                        }, {
+                            name: '_flag',
+                            type: 4
+                        }, {
+                            name: '_map',
+                            type: 5
+                        }]
+                    }
+                }
+            });
+
+            map.resume();
+            map.on('end', done);
+        });
+
+        it('can get a map', function (done) {
+
+            var map = client.getCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map'
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                expect(reply).to.have.property('type', 3);
+                expect(reply).to.have.property('value').that.is.an('object');
+            });
+            
+            map.on('end', done);
+        });
+
+        it('can update a counter within a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_counter',
+                                type: 1
+                            },
+                            counter_op: {
+                                increment: 5
+                            }
+                        }]
+                    }
+                },
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(1);
+                expect(reply.map_value[0]).to.have.deep.property('field.name', '_counter');
+                expect(reply.map_value[0]).to.have.deep.property('field.type', 1);
+                expect(reply.map_value[0]).to.have.property('counter_value');
+                expect(reply.map_value[0].counter_value.toNumber()).to.equal(5);
+            });
+
+            map.on('end', done);
+        });
+
+        it('can update a set within a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_set',
+                                type: 2
+                            },
+                            set_op: {
+                                adds: ['one', 'two']
+                            }
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(2);
+                expect(reply.map_value).to.include({ field: { name: '_set', type: 2 }, set_value: ['one', 'two'] });
+            });
+
+            map.on('end', done);
+        });
+
+        it('can update a register within a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_register',
+                                type: 3
+                            },
+                            register_op: 'testing'
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(3);
+                expect(reply.map_value).to.include({ field: { name: '_register', type: 3 }, register_value: 'testing' });
+            });
+
+            map.on('end', done);
+        });
+
+        it('can update a flag within a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_flag',
+                                type: 4
+                            },
+                            flag_op: 1
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(4);
+                expect(reply.map_value).to.include({ field: { name: '_flag', type: 4 }, flag_value: true });
+            });
+
+            map.on('end', done);
+        });
+
+        it('can update a map within a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        updates: [{
+                            field: {
+                                name: '_map',
+                                type: 5
+                            },
+                            map_op: {
+                                updates: [{
+                                    field: {
+                                        name: '_test',
+                                        type: 4
+                                    },
+                                    flag_op: 2
+                                }]
+                            }
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+            
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                context = reply.context;
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(5);
+                expect(reply.map_value).to.include({ field: { name: '_map', type: 5 }, map_value: [{ field: { name: '_test', type: 4 }, flag_value: false }] });
+            });
+
+            map.on('end', done);
+        });
+
+        it('can remove a field from a map', function (done) {
+
+            var map = client.updateCrdt({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map',
+                op: {
+                    map_op: {
+                        removes: [{
+                            name: '_map',
+                            type: 5
+                        }]
+                    }
+                },
+                context: context,
+                return_body: true
+            });
+
+            map.on('data', function (reply) {
+
+                expect(reply).to.have.property('context').that.is.an.instanceof(Buffer);
+                expect(reply).to.have.property('map_value').that.is.an.instanceof(Array);
+                expect(reply.map_value).to.have.length(4);
+                expect(reply.map_value).to.not.include({ field: { name: '_map', type: 5 }, map_value: [{ field: { name: '_test', type: 4 }, flag_value: false }] });
+            });
+
+            map.on('end', done);
+        });
+
+        after(function (done) {
+
+            var map = client.del({
+                bucket: '_test_maps',
+                type: '_test_crdt_map',
+                key: '_test_map'
+            });
+
+            map.resume();
+            map.on('end', done);
+        });
     });
 });
